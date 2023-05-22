f816cfb3fa1bc55620eb1bb9d147165243d1c7f2
Osman Oguzhan SAVAS <osmanoguzhansavas@gmail.com>
2023-05-22 17:10:49 +0300

    19. dosya bitirildi.

--- 018.cpp
+++ 018.cpp
@@ -6,17 +6,17 @@
 
 // ```
 
-#include <iostream>
+// #include <iostream>
 
-int main()
-{
-	const int x = 20;
-	//& olmadığı için y x'i refere etmez ve arı bir değişken olarak arttırılabilir
-	auto y = x; //int y = x
-	++y;
+// int main()
+// {
+// 	const int x = 20;
+// 	//& olmadığı için y x'i refere etmez ve arı bir değişken olarak arttırılabilir
+// 	auto y = x; //int y = x
+// 	++y;
 
-	std::cout << y << " " << x << "\n";
-}
+// 	std::cout << y << " " << x << "\n";
+// }
 // ```
 
 // [ödevin cevabı](https://www.youtube.com/watch?v=AYVwPKvnmPI)
---
+++ 019.cpp
@@ -0,0 +1,27 @@
+// #### Aşağıdaki C++ programı hakkında yorum yapınız:
+
+// + sentaks hatası
+// + tanımsız davranış
+// + derleyiciye göre değişir
+// + ekrana şunu yazar: 
+
+
+// ```
+// #include <iostream>
+
+// int main()
+// {
+// 	int a[] = { 10, 20, 30, 40 };
+// 	auto p = a; //int *p = a
+// 	auto &r = p; //int *&r = p
+	
+// 	++r;
+// 	std::cout << *r << "\n";
+// 	++p;
+
+// 	std::cout << *r << "\n";
+	
+// }
+// ```
+
+// [ödevin cevabı](https://www.youtube.com/watch?v=AYVwPKvnmPI)
